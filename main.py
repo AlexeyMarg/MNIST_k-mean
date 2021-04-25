4f1efaaaa329e4f7a7fd37b1c81f8f19ca5914f9
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2021-04-25 16:36:49 +0300

    testing in main

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import  k_mean
 
 if __name__ == '__main__':
-    pass
+    train_set_path = 'dataset/mnist_train.csv'
+    test_set_path = 'dataset/mnist_test.csv'
+
+    classifier = k_mean.k_mean(10, 28 * 28, 255)
+    classifier.test(train_set_path, test_set_path)
