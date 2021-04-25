af66015db465950c27cd7ad4d401d76a8dff2fac
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2021-04-25 14:41:48 +0300

    class k_maen create

---
+++ k-mean.py
@@ -0,0 +1,12 @@
+class k_mean:
+    def __init__(self, k, dimension, max_deviation):
+        pass
+
+    def load_data(self, path):
+        pass
+
+    def rmse(self, loaded_vector, predict_vector):
+        pass
+
+    def run(self, predict_vector):
+        pass
